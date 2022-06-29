c1b513b5e8adbe8343dbd6b7901c2769bda71b3e
Alex Kysorets <Alexkisorec@gmail.com>
2022-06-29 20:39:25 +0300

    + second regex task

---
+++ regex/second_task.rb
@@ -0,0 +1,10 @@
+def check_name(str)
+  print(str =~ /^(\p{Lu})(\p{Ll}{1,14})?((-)((\p{Lu})(\p{Ll}{1,14})?))?$/)
+end
+
+check_name("Oleksandr")
+
+# Oleksandr-Oleksandr
+# Анна-Марія
+# Анна-Марыя
+# أمين-أمين
