d2511fd21c4b0d370cab0e2baa73b30209aeb828
forget-me-not-nc <forget.me.not.x64@gmail.com>
2022-06-29 20:01:00 +0300

    Complete Task 1. Add regexp matchers methods

---
+++ 29_06_2022/task_1.rb
@@ -0,0 +1,26 @@
+#-----------------------#
+
+def capital_letter_index(str)
+  (str =~ /[A-Z]/).to_s
+end
+
+def lowercase_letter_index(str)
+  (str =~ /[a-z]/).to_s
+end
+
+def number_index(str)
+  (str =~ /\d/).to_s
+end
+
+def special_character_index(str)
+  (str =~ /[^[:alnum:]]/).to_s
+end
+
+#-----------------------#
+
+line = gets.to_s
+
+puts capital_letter_index(line) + '_' +
+       lowercase_letter_index(line) + '_' +
+       number_index(line) + '_' +
+       special_character_index(line)
