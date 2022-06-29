c32bc6f545ca1723b76fa5b9c1d24489b19bf917
forget-me-not-nc <forget.me.not.x64@gmail.com>
2022-06-29 20:02:00 +0300

    Complete Task 2. Create regexp for names

--- 29_06_2022/task_2.rb
+++ 29_06_2022/task_2.rb
@@ -1,1 +1,12 @@
+#/((?<=\p{Z})|(?<=\p{P})|(?<=^))\p{Lu}\p{Ll}{0,14}((?=\p{Z})|(?=\p{P})|(?=$))/  #Only name
+#/((?<=\p{Z})|(?<=\p{P})|(?<=^))\p{Lu}\p{Ll}{0,14}-\p{Lu}\p{Ll}{0,14}((?=\p{Z})|(?=\p{P})|(?=$))/ #Name with hyphen
+#
+#/((?<=\p{Z})|(?<=\p{P})|(?<=^))\p{Lu}\p{Ll}{0,14}-\p{Lu}\p{Ll}{0,14}((?=\p{Z})|(?=\p{P})|(?=$))| #Simple name and name with hyphen
+# ((?<=\p{Z})|(?<=\p{P})|(?<=^))\p{Lu}\p{Ll}{0,14}((?=\p{Z})|(?=\p{P})|(?=$))[^-]/
+def find_names(str)
+  str.scan(/((?<=\p{Z})|(?<=\p{P})|(?<=^))\p{Lu}\p{Ll}{0,14}-\p{Lu}\p{Ll}{0,14}((?=\p{Z})|(?=\p{P})|(?=$))|((?<=\p{Z})|(?<=\p{P})|(?<=^))\p{Lu}\p{Ll}{0,14}((?=\p{Z})|(?=\p{P})|(?=$))[^-]/)
+end
 
+text = gets.to_s
+
+puts find_names(text)
