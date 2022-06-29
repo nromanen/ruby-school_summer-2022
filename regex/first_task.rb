598bd91c5d2a85fa41716592d7ca3705ea1791f0
Alex Kysorets <Alexkisorec@gmail.com>
2022-06-29 16:10:19 +0300

    + first regex task

---
+++ regex/first_task.rb
@@ -0,0 +1,22 @@
+def contains_upper_letter(str)
+  str =~ /[A-Z]/
+end
+
+def contains_lower_letter(str)
+  str =~ /[a-z]/
+end
+
+def contains_number(str)
+  str =~ /\d/
+end
+
+def contains_special_character(str)
+  str =~ /\W/
+end
+
+def do_regex(str)
+  print("#{contains_upper_letter(str)}_#{contains_lower_letter(str)}_#{contains_number(str)}_#{contains_special_character(str)}")
+end
+
+do_regex('abc1')
+do_regex('#####')
