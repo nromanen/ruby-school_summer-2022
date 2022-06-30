ed673f8a813c4d6952a297f6816abbdb1949307e
Lalaloopsy1133 <Lalaloopsy.poopsik@gmail.com>
2022-06-30 11:10:34 +0300

    add Task

---
+++ Task/task_1.rb
@@ -0,0 +1,21 @@
+# Task1
+
+def big_letters (str)
+    str =~ /[A-Z]/
+  end
+  
+  def small_letters (str)
+    str =~ /[a-z]/
+  end
+
+  def numbers (str)
+    str =~ /[0-9]/
+  end
+  
+  def special_symbols (str)
+    srt =~ /\W/
+  end
+
+  word_1 = ('abc1')
+
+  puts "#{big_letters(word_1)}_#{small_letters(word_1)}_#{numbers(word_1)}_#{special_symbols(word_1)}"
