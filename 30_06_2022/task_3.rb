66120858cda2eba31252100cfa3cb0e2117d2600
forget-me-not-nc <forget.me.not.x64@gmail.com>
2022-06-30 22:59:19 +0300

    Added solution for task_3

--- 30_06_2022/task_3.rb
+++ 30_06_2022/task_3.rb
@@ -1,1 +1,5 @@
+def say_hello(name, city, state)
+  "Hello, #{name.join(' ')}!".concat(' Welcome to ', city, ', ', state, '!')
+end
 
+p say_hello(%w[John Smith], 'Phoenix', 'Arizona') #expect 'Hello, John Smith Smith! Welcome to Phoenix, Arizona!'
