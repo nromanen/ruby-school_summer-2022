5660612b80f912ed62febfe8846e5b1ade270124
KERRAT <v.vlad.demid@gmail.com>
2022-06-30 03:24:31 +0300

    task_1 interface

--- lecture-1/task_1.rb
+++ lecture-1/task_1.rb
@@ -13,18 +13,42 @@ def print_array (arr)
   puts "\n"
 end
 
-string = ")($T$h$!i^s Ruby style guide recommends (#323$1!2) best practices so that real-world Ruby programmers (#323$1!2) can write code that can be maintained (#323$1!2) by other real-world Ruby programmers. A style guide that reflects (#323$1!2) real-world usage gets used, while a style guide that holds to an ideal that has been (#323$1!2) rejected by the people it is 24supposed to help risks not getting used at 2all - no matter how good it is."
+conf = 1
+while true
+  puts 'Enter number (1 -- enter own string, 2 -- use pre-installed string,  0 -- exit)'
+  conf = gets.chomp.to_i
 
+  case conf
+  when 1
+    string = gets
+  when 2
+    string = ')($T$h$!i^s Ruby style guide recommends (#323$1!2) best practices so that real-world Ruby programmers (#323$1!2) can write code that can be maintained (#323$1!2) by other real-world Ruby programmers. A style guide that reflects (#323$1!2) real-world usage gets used, while a style guide that holds to an ideal that has been (#323$1!2) rejected by the people it is 24supposed to help risks not getting used at 2all - no matter how good it is.'
+  when 0
+    break
+  else
+    puts "what?"
+  end
 
-print_array get_start_ids_of_matches(/[A-Z]/,string)
-puts "\n"
-
-print_array get_start_ids_of_matches(/[a-z]/,string)
-puts "\n"
-
-print_array get_start_ids_of_matches(/[\d]/,string)
-puts "\n"
-
-print_array get_start_ids_of_matches(/[^A-Za-z0-9 ]{1,10}/,string)
-puts "\n"
-
+  while true
+    puts 'Enter number (1 -- get capital letters, 2 -- lower letters, 3 -- digits, 4 -- special characters  0 -- exit)'
+    conf1 = gets.chomp.to_i
+    case conf1
+    when 1
+      print_array get_start_ids_of_matches(/[A-Z]/,string)
+      puts "\n"
+    when 2
+      print_array get_start_ids_of_matches(/[a-z]/,string)
+      puts "\n"
+    when 3
+      print_array get_start_ids_of_matches(/[\d]/,string)
+      puts "\n"
+    when 4
+      print_array get_start_ids_of_matches(/[^A-Za-z0-9 ]{1,10}/,string)
+      puts "\n"
+    when 0
+      break
+    else
+      puts "what?"
+    end
+  end
+end
