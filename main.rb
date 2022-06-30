7d1a0297d8b01e983b9b46a3f8e90ab602dae1fa
AntonTmk <35644424+AntonTmk@users.noreply.github.com>
2022-06-30 14:10:12 +0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -9,17 +9,7 @@ io+=1
 end
 
 
-def ip_address?(str)
-   !!(str =~ /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/)
- end
- print "\n \n", ip_address?("192.168.1.1")  # returns true
- print ip_address?("0000.0000") 
 
- Line = Struct.new(:name)
-LOG_FORMAT = /([A-Za-z]+-.[A-Za-z]+)/
-print "\n \n \n true \n \n"
-#end
-#
 def names1?(str)
   !!(str =~ /[A-Z][a-z]+\-[A-Z][a-z]+$/)
 end
