9d08d56c6335ff6037adb869383d4055980089af
Alex Kysorets <Alexkisorec@gmail.com>
2022-06-30 17:29:54 +0300

    * refactor methods names

--- regex/first_task.rb
+++ regex/first_task.rb
@@ -1,21 +1,21 @@
-def contains_upper_letter(str)
+def regex_upper_letter(str)
   str =~ /[A-Z]/
 end
 
-def contains_lower_letter(str)
+def regex_lower_letter(str)
   str =~ /[a-z]/
 end
 
-def contains_number(str)
+def regex_number(str)
   str =~ /\d/
 end
 
-def contains_special_character(str)
+def regex_special_character(str)
   str =~ /[^[:alnum:]]/
 end
 
 def do_regex(str)
-  print("#{contains_upper_letter(str)}_#{contains_lower_letter(str)}_#{contains_number(str)}_#{contains_special_character(str)}")
+  print("#{regex_upper_letter(str)}_#{regex_lower_letter(str)}_#{regex_number(str)}_#{regex_special_character(str)}")
 end
 
 do_regex('abc1')
