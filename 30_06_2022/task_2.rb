8c5cc249938e6c9014dc1e88f7e253fe0de70f27
forget-me-not-nc <forget.me.not.x64@gmail.com>
2022-06-30 20:46:04 +0300

    Added solution for task_2

--- 30_06_2022/task_2.rb
+++ 30_06_2022/task_2.rb
@@ -1,1 +1,11 @@
+def square_sum(numbers)
+  res = 0
+  numbers.each{ |elem| res += elem**2 }
+  res
+end
 
+p square_sum([1, 2]) # expected 5
+p square_sum([0, 3, 4, 5]) # expected 50
+p square_sum([]) # expected 0
+p square_sum([-1, -2]) # expected 5
+p square_sum([-1, 0, 1]) # expected 2
