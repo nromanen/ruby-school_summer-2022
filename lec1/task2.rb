31d29dd9d70396c9b81e0a0d499931c88e71759e
jamrockshuffle <70891061+jamrockshuffle@users.noreply.github.com>
2022-07-01 14:27:34 +0300

    Fixed task2 at lec1 folder.

--- lec1/task2.rb
+++ lec1/task2.rb
@@ -2,9 +2,9 @@ def match(str)
   puts "#{str} - #{'not ' unless str =~ /^\p{Lu}\p{Ll}{0,14}(-\p{Lu}\p{Ll}{0,14})?$/}match"
 end
 
-puts match('Ффффффффффффффф')
-puts match('І-Ф')
-puts match('Анна1')
-puts match('Hello')
-puts match('Anna-Henrietta')
-puts match('Anna-henrietta')
+match('Ффффффффффффффф')
+match('І-Ф')
+match('Анна1')
+match('Hello')
+match('Anna-Henrietta')
+match('Anna-henrietta')
