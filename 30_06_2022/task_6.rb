919b70d51ea1d74abeddf694ffadaa1d114c99ce
forget-me-not-nc <forget.me.not.x64@gmail.com>
2022-07-01 14:30:38 +0300

    Added solution for task_6. Refactored regexp task_2

--- 30_06_2022/task_6.rb
+++ 30_06_2022/task_6.rb
@@ -1,1 +1,15 @@
+def get_squared_or_rooted(num)
+  root = Math.sqrt(num)
+  (root % 1).zero? ? root.floor : num**2
+end
+
+def square_or_square_root(arr)
+  arr.map{ |elem| get_squared_or_rooted(elem) }
+end
+
+p square_or_square_root([4, 3, 9, 7, 2, 1]) # expected [2, 9, 3, 49, 4, 1]
+p square_or_square_root([100, 101, 5, 5, 1, 1]) # expected [10, 10201, 25, 25, 1, 1]
+p square_or_square_root([1, 2, 3, 4, 5, 6]) # expected [1, 4, 9, 2, 25, 36]
+
+
 
