57b211d2d49dcee6794570647167e6630e819212
Den Burak <den.burak04@gmail.com>
2022-07-04 11:52:32 +0300

    Add task_2

---
+++ task_02/task_2.rb
@@ -0,0 +1,17 @@
+def square_sum(numbers)
+
+    result = 0
+
+    for numbers in numbers
+      result += numbers**2
+    end
+
+    p result
+
+  end
+
+square_sum([1, 2]) # 5
+square_sum([0, 3, 4, 5]) # 50
+square_sum([]) # 0
+square_sum([-1, -2]) # 5
+square_sum([-1, 0, 1]) # 2
