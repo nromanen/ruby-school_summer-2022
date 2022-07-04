611c23a115be3abf0e7b9db408d258cb05e657a2
Lalaloopsy1133 <Lalaloopsy.poopsik@gmail.com>
2022-07-04 14:10:56 +0300

    fix array_task_1

--- Array/array_task_1.rb
+++ Array/array_task_1.rb
@@ -1,11 +1,11 @@
 def powers_of_two(n)
-    array = []
-
-    for i in n do 
-        array[i] = 2 ** i
-        
-    end 
-    return array
-
-
+    array=[]
+        for i in 0..n do
+            array[i]=2 ** i
+          i += 1
+        end
+      
+    puts array 
+      
     end
+    powers_of_two(2)
