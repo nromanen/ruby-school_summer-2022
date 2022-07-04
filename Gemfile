d9fb7d7cc4db9d531f0140eb8889221798e90c51
Bohdana Havaleshko <bohdana.shelley@gmail.com>
2022-07-04 23:51:06 +0300

    complete task 4

--- Gemfile
+++ Gemfile
@@ -6,3 +6,4 @@ source 'https://rubygems.org'
 
 gem 'rspec'
 gem 'rubocop'
+gem 'test-unit'
--- inventory.rb
+++ inventory.rb
@@ -1,4 +1,14 @@
 module Inventory
   def warehouse(array)
+    result = {}
+    array.each do |value|
+      product, amount = value.split(/ *- */)
+      if result[:"#{product}"].nil?
+        result[:"#{product}"] = amount.to_i
+      else
+        result[:"#{product}"] += amount.to_i
+      end
+    end
+    result
   end
 end
