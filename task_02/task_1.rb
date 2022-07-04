649e8b61f6a5bb3db88c4a475212dce1149fa5dc
Den Burak <den.burak04@gmail.com>
2022-07-04 11:37:54 +0300

    Add task_1

---
+++ task_02/task_1.rb
@@ -0,0 +1,19 @@
+def powers_of_two(n)
+
+    if n < 0 
+        p "Your number < 0"
+    else
+
+        array = []
+
+        (0..n).each do |num|
+            #array.append "2^#{2**num}" # => ["2^1", "2^2", "2^4"]
+            array.append 2**num
+        end
+        
+        p array
+
+    end
+end
+  
+powers_of_two(0)
