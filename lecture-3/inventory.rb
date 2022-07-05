5370bbd897a00495f12594028178c59849883b97
KERRAT <v.vlad.demid@gmail.com>
2022-07-05 14:48:51 +0300

    transfer to func

--- lecture-3/inventory.rb
+++ lecture-3/inventory.rb
@@ -1,20 +1,23 @@
-
-
 module Inventory
-
   def warehouse(array)
     rez = {}
     array.each do |line|
-      matches = line.match(/([^ -]{1,10})( -|-|- | - )(\d{1,10})/)
-      key = matches[1].to_sym
-      value = matches[3].to_i
-
-      if rez.key?(key)
-        rez[key] = rez[key] + value
-      else
-        rez.merge!(Hash[matches[1].to_sym => matches[3].to_i])
-      end
+      format_line(line, rez)
     end
     rez
+  end
+
+  private
+
+  def format_line(line, rez)
+    matches = line.match(/([^ -]{1,10})( -|-|- | - )(\d{1,10})/)
+    key = matches[1].to_sym
+    value = matches[3].to_i
+
+    if rez.key?(key)
+      rez[key] = rez[key] + value
+    else
+      rez.merge!(Hash[matches[1].to_sym => matches[3].to_i])
+    end
   end
 end
