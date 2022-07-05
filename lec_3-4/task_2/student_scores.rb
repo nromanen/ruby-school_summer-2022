e594724168d5c3920f978b759726cec72ef1c0fe
jamrockshuffle <70891061+jamrockshuffle@users.noreply.github.com>
2022-07-05 20:07:04 +0300

    Added sort for student_scores.rb

--- lec_3-4/task_2/student_scores.rb
+++ lec_3-4/task_2/student_scores.rb
@@ -4,17 +4,19 @@ def students_by_scores(max, low, up, students)
   scores = Hash[:top => [], :middle => [], :bottom => []]
   split_scores = students.split(/[\s,']/).each_slice(2).to_a
   split_scores.map { |student, score| [student, score.to_i]}
+              .sort {|student, score| student[1] <=> score[1]}
+              .reverse
               .each do |e|
     if min_range.include?(e[1])
       scores[:bottom].push(e[0])
     elsif max_range.include?(e[1])
-      scores[:top].push(e[0])
+      scores[:top].push(e[0]).sort
     else
-      scores[:middle].push(e[0])
+      scores[:middle].push(e[0]).sort
     end
   end
   puts scores
 end
 
 students_by_scores(100, 15, 10,
-                   "S1,5\nS2,18\nS3,45\nS4,100\nS5,90\nS6,94\nS7,1")
+                   "S1,5\nS2,18\nS3,45\nS4,90\nS5,100\nS6,94\nS7,1")
