4d6e881fd33ff16d60a21bafe311f4a0ce5e0b20
jamrockshuffle <70891061+jamrockshuffle@users.noreply.github.com>
2022-07-06 12:33:31 +0300

    Fixed minor code mistakes

--- lec_3-4/task_2/student_scores.rb
+++ lec_3-4/task_2/student_scores.rb
@@ -1,24 +1,24 @@
 def students_by_scores(max, low, up, students)
+  scores = Hash[top: [], middle: [], bottom: []]
   min_range = 0 .. max * low / 100
   max_range = max - up .. max
-  scores = Hash[top: [], middle: [], bottom: []]
   split_scores = students.split(/[\s,']/)
                          .each_slice(2)
                          .to_a
-  split_scores.map { |student, score| [student, score.to_i]}
-              .sort {|student, score| student[1] <=> score[1]}
+  split_scores.map { |student, score| [student, score.to_i] }
+              .sort { |student, score| student[1] <=> score[1] }
               .reverse
-              .each do |e|
-    if min_range.include?(e[1])
-      scores[:bottom].push(e[0])
-    elsif max_range.include?(e[1])
-      scores[:top].push(e[0])
-    else
-      scores[:middle].push(e[0])
-    end
-  end
+              .each {  |e|
+                if min_range.include?(e[1])
+                  scores[:bottom].push(e[0])
+                elsif max_range.include?(e[1])
+                  scores[:top].push(e[0])
+                else
+                  scores[:middle].push(e[0])
+                end
+              }
   puts scores
 end
 
 students_by_scores(100, 15, 10,
-                   "S1,5\nS2,18\nS3,45\nS4,90\nS5,100\nS6,94\nS7,1")
+                   "S1,5\nS2,18\nS3,45\nS4,90\nS5,100\nS6,94\nS7,1\nS8,13")
