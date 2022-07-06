b6760bb4c6be0205a2bf02dd86ca43c5da9d6a6a
Den Burak <den.burak04@gmail.com>
2022-07-06 11:55:34 +0300

    Fix and update special symbol function

--- task_01/regex_1.rb
+++ task_01/regex_1.rb
@@ -1,28 +1,30 @@
+# frozen_string_literal: true
+
 # Get string
 print 'Enter your words: '
 regex = gets
 
 # Get index
 def first_up_latter(regex)
-    regex =~ /[A-Z]/
+  regex =~ /[A-Z]/
 end
 
 def second_lower_letter(regex)
-    regex =~ /[a-z]/
+  regex =~ /[a-z]/
 end
 
 def number(regex)
-    regex =~ /\d/
+  regex =~ /\d/
 end
 
 def special_symbol(regex)
-    regex =~ /\W/
+  regex =~ %r{[.,!@"'#$%^&*;:()\-+=_`~/\\{}\[\]|<>]}
 end
 
 #  Search in string
 def regex_func(regex)
-    "#{first_up_latter(regex)}_#{second_lower_letter(regex)}_#{number(regex)}_#{special_symbol(regex)}"
+  p "#{first_up_latter(regex)}_#{second_lower_letter(regex)}_#{number(regex)}_#{special_symbol(regex)}"
 end
 
 # Result
-puts regex_func(regex)
+regex_func(regex)
