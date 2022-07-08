6bcdf2355de44231c382bef7d13ee89d92c97fab
Alexander Sahan <mortar5900@gmail.com>
2022-07-09 01:22:39 +0300

    Edit task_3/students.rb

--- task_3/students.rb
+++ task_3/students.rb
@@ -1,40 +1,30 @@
-def classifier(grade, max_grade, deviation_percent, min_percent)
+def csv_to_array(csv_students)
+    students = []
+    csv_students.split(/ *\n */).map do |item|
+        name, grade = item.split(/ *, */)
+        students.push({name: name, grade: grade.to_i})
+    end
+    students
+end
+
+def sort_students(students)
+    sorted_students = students.sort do |a, b|
+            b[:grade].to_i - a[:grade].to_i == 0 ? a[:name] <=> b[:name] : b[:grade].to_i - a[:grade].to_i    
+    end
+    sorted_students
+end
+
+def ranker(grade, max_grade, deviation_percent, min_percent)
     return :top if grade >= max_grade - max_grade * deviation_percent * 0.01
     return :bottom if grade < max_grade * min_percent * 0.01
     :middle
 end
 
-def classificate_students(students_csv, max_grade, deviation_percent, min_percent)
-    students = {top: [], middle: [], bottom: []}
-    students_csv.split(/ *\n */).map do |item|
-        name, grade = item.split(/ *, */)
-        tmp_student = {name:, grade:}
-        tmp_student[:name] = name
-        tmp_student[:grade] = grade.to_i
-        students[classifier(grade.to_i, max_grade, deviation_percent, min_percent)].push(tmp_student)
+def rank_students(csv_text, max_grade, deviation_percent, min_percent)
+    ranked_students = {top: [], middle: [], bottom: []}
+    students = sort_students(csv_to_array(csv_text))
+    students.each do |item|
+        ranked_students[ranker(item[:grade], max_grade, deviation_percent, min_percent)].push(item[:name])
     end
-    sort_students(students)
-end
-
-def sort_students(students)
-    sorted_students = {top: [], middle: [], bottom: []}
-    students.each do |key, value|  
-        value = value.sort do |a, b|
-            if b[:grade].to_i - a[:grade].to_i == 0
-               a[:name] <=> b[:name]
-            else
-                b[:grade].to_i - a[:grade].to_i
-            end      
-        end
-        sorted_students[key.to_sym] = value
-    end
-    sorted_students
-end
-
-def delete_grades(students_with_grades)
-    names = Hash.new(0)
-    students_with_grades.each do |key, array|
-        names[key.to_sym] = array.map{|item| item = item[:name]}
-    end
-    names
+    ranked_students
 end
