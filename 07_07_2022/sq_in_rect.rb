8d9b6232d7b0923445b99ecd23f1c259c1af1b15
forget-me-not-nc <forget.me.not.x64@gmail.com>
2022-07-09 15:11:35 +0300

    Added solution for Rectangle into Squares task

--- 07_07_2022/sq_in_rect.rb
+++ 07_07_2022/sq_in_rect.rb
@@ -1,1 +1,20 @@
+def sq_in_rect(lng, wdth)
+  if lng == wdth
+    nil
+  else
+    res = []
+    while lng > 0 && wdth > 0
+      if lng <= wdth
+        res.push(lng)
+        wdth -= lng
+      else
+        res.push(wdth)
+        lng -= wdth
+      end
+    end
+    res
+  end
+end
 
+p sq_in_rect(5, 5) #expected nil
+p sq_in_rect(5, 3) #expected [3, 2, 1, 1]
