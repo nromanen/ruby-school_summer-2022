e842b52fd43d38b3f6ef692e988c01621cf70cd7
Alexander Sahan <mortar5900@gmail.com>
2022-07-10 14:59:45 +0300

    Edit task_2/name.rb

--- task_2/name.rb
+++ task_2/name.rb
@@ -1,25 +1,25 @@
-def is_name(name)
-    name_pattern = /^[А-Я,І]{1}[а-я,і]{0,14}$/
-    return name =~ name_pattern ? true : false
+def name?(name)
+    return name =~ /^\p{Upper}{1}\p{Lower}{0,14}$/ ? true : false
 end 
 
-def is_doublename(name)
+def doublename?(name)
     if name =~ /\-/
         i = name =~ /\-/
-        return (is_name(name[0...i]) and is_name(name[i+1..-1]))? true : false
+        return (name?(name[0...i]) and name?(name[i+1..-1]))? true : false
     end
 end 
 
-names = 
-[
+def name_validator(names)
+    names.each do |name|
+        puts (name?(name) or doublename?(name))? "#{name} - Valid" : "#{name} - Invalid"
+    end
+end
+
+name_validator([
     "Анна",
     "Анна2",
     "Анна-Марія",
     "Нннннннннн",
     "Забагатосимволів",
     "І-Ф"
-]
-
-names.each do |name|
-    puts (is_name(name) or is_doublename(name))? "#{name} - Valid" : "#{name} - Invalid"
-end
+])
