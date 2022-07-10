6405b51e173c7419da2f023f3f57a76f25f46f25
jamrockshuffle <70891061+jamrockshuffle@users.noreply.github.com>
2022-07-10 12:06:42 +0300

    Added merge_into_2048.rb

---
+++ lec_6/merge_into_2048.rb
@@ -0,0 +1,20 @@
+def merge(arr)
+
+  start_i = 0
+  end_i = 1
+
+  while end_i < arr.length do
+    if arr[start_i] == arr[end_i]
+      arr[start_i] = arr[start_i] + arr[end_i]
+      arr[end_i] = 0
+      start_i+=1
+    end
+    end_i+=1
+  end
+  print("#{arr.sort.reverse}\n")
+end
+
+merge([2,0,2,2]) # 4,2,0,0
+merge([2,0,2,4]) # 4,4,0,0
+merge([4,4,0,4]) # 8,4,0,0
+merge([2,2,4,16]) # 16,4,4,0
