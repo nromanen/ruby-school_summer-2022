c63b07e7fd72a981bc998f5b13de70e61fe6aae1
Lalaloopsy1133 <Lalaloopsy.poopsik@gmail.com>
2022-07-11 13:07:51 +0300

    add inventory_

--- Hash/inventory.rb
+++ Hash/inventory.rb
@@ -1,4 +1,14 @@
 module Inventory
   def warehouse(array)
+    res = Hash.new
+    array.map{ |x| x.scan(/\w+/) }
+         .each { |x|
+           if res[x[0].to_sym].nil?
+             res[x[0].to_sym] = x[1].to_i
+           else
+             res[x[0].to_sym] += x[1].to_i
+           end
+         }
+    res
   end
 end
