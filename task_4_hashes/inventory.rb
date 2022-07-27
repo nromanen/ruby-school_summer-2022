e6c69cfc572ed724fd12b7e8d34a8314a3186e29
david <datskodavid@gmail.com>
2022-07-27 19:14:52 +0300

    add task_4

---
+++ task_4_hashes/inventory.rb
@@ -0,0 +1,16 @@
+module Inventory
+  def warehouse(array)
+    sold = Hash.new
+    array.map{ |elem| elem.scan(/\w+/) }
+         .map{ |elem| [elem[0].to_sym, elem[1].to_i] }
+         .each{
+           |elem|
+           if sold.has_key?(elem[0])
+             sold[elem[0]] = sold[elem[0]] + elem[1]
+           else
+             sold.store(elem[0], elem[1])
+           end
+         }
+    sold
+  end
+end
