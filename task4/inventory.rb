80c8925b0094b30ecd6e8a7e5c54da78068353d9
Bohalius <72955867+Bohalius@users.noreply.github.com>
2022-07-30 10:55:29 +0300

    Add task 4

---
+++ task4/inventory.rb
@@ -0,0 +1,15 @@
+module Inventory
+  def warehouse(myArray)
+    res = Hash.new(0)
+    myArray.map{ |val| val.split(/ *- */) }
+           .map{ |val| [val[0].to_sym, val[1].to_i] }
+           .each{|val|
+             if res.key?(val[0])
+               res[val[0]] += val[1]
+             else
+               res.store(val[0], val[1])
+             end
+           }
+    return res
+  end
+end
